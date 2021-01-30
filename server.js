2c64531e78de05267844c5d23cce8c4d7f44e360
Loren Rachelle Hicks <lorenhicks@gmail.com>
2021-01-30 04:21:08 -0500

    Express Server Init

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res) => {
   console.log(req.headers);
